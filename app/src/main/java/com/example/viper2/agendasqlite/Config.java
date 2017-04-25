8e65831a9e1b6f600e35618ff3a3cfce6ded3e08
DavidQM <alexis.quintero@udea.edu.co>
2017-04-25 08:05:08 -0500

    Comunicacion_DB sin Pruebas 25042017 - TodoElCodigoListo

--- app/src/main/java/com/example/viper2/agendasqlite/Config.java
+++ app/src/main/java/com/example/viper2/agendasqlite/Config.java
@@ -7,6 +7,9 @@ package com.example.viper2.agendasqlite;
 public class Config {
         // localhost = http://192.168.137.1
     public static final String URL_ADD = "http://192.168.137.1/agenda/create_contact.php";
+    public static final String URL_UPDATE = "http://192.168.137.1/agenda/update_contact.php";
+    public static final String URL_DELETE = "http://192.168.137.1/agenda/update_contact.php?nombre=";
+    public static final String URL_GET_CONTACT = "http://192.168.137.1/agenda/get_contact_details.php";
     /*
     public static final String URL_GET_ALL = "http://192.168.137.1/agenda/get_all_books.php";
     public static final String URL_DELETE = "http://192.168.137.1/agenda/delete_book.php?libro=";
